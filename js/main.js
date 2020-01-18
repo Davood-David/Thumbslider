17ec350fba1532201e8920767049047fe575c5b6
Davood Hadiannejad <davoodhadiannejad@gmail.com>
2020-01-18 21:16:33 +0100

    finished project

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function(){
+  
+   //  Declare Variables 
+    var totalWidth = 0;
+    var positions = new Array();
+
+    $('#slides .slide').each(function(i){
+        // Get slider widths
+        positions[i] = totalWidth;
+        totalWidth += $(this).width();
+
+        // Check widths
+        if(!$(this).width()){
+            alert('Please add a width to your images');
+            return false;
+        }
+    });
+
+    // Set Width
+    $('#slides').width(totalWidth);
+    // Menu item click handler
+    $('#menu ul li a').click(function(e, keepScroll){
+        // Remove active class and add inactive
+        $('li.product').removeClass('active')
+        // Add active class to parent
+        $(this).parent().addClass('active');
+
+        var pos =  $(this).parent().prevAll('.product').length;
+
+        $('#slides').stop().animate({marginLeft:-positions[pos]+ 'px'},450);
+
+        // Prevent default
+        e.preventDefault();
+
+        //Stop autoScroll
+        if(!autoScroll)  clearInterval(invl);
+    });
+
+    // Make first image active
+    $('#menu ul li.product:first').addClass('active').siblings().addClass('inactive');
+
+    // Auto Scroll
+    var current =1;
+    function autoScroll(){
+        if(current == -1) return false;
+        $('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click', [true]);
+        current++;
+    }
+
+    // Duration for auto scroll
+
+    var duration = 10;
+    var itvl = setInterval(function(){
+        autoScroll()
+    }, duration*1000);
+
+});
